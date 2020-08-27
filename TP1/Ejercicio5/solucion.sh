a9b7cd1267c1acf79c2409fa66402db0619f45b2
marcosaleL <alelazarte55@gmail.com>
2020-08-27 10:21:52 -0300

    Nuevos ejercicios

--- TP1/Ejercicio5/solucion.sh
+++ TP1/Ejercicio5/solucion.sh
@@ -9,3 +9,15 @@ cd /home/ale/Desktop/SO2020/TP1/Ejercicio5
 #    #le voy a cambiar el nombre a los archivos solamente
 #    filename=$(basename $carpetas)
 #done
+
+#---------Hecho por Moran----------
+
+#!/bin/bash
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+for i in $( find "$1" -type f ); do
+    filename=`basename "$i"`
+    path=`dirname "$i"`
+    name_tr=$(echo "$filename" | tr A-Z a-z | tr -s ' ' | tr ' ' '_')
+    mv $i "$path"/"$name_tr"
+done
--- TP1/Ejercicio8/solucion.sh
+++ TP1/Ejercicio8/solucion.sh
@@ -1,13 +1,18 @@
-#my_root_depth=$(echo $(realpath "$1" | tr -cd '/' | wc -c))
-#for i in $(find $1 -type d); do
-#    actual=$(echo $(realpath "$i" | tr -cd '/' | wc -c))
-#    result=$(($actual - $my_root_depth))
-#    spaces=""
-#    for ((j=0;j<result;j++)); do
-#        spaces=$spaces"   "
-#    done
-#    filename=$(basename $i)
-#    echo $spaces \* $filename
-#done
+#!/bin/bash
 
-echo $(realpath $1 | tr -cd '/' | wc -c)
+old_ifs=$IFS
+IFS='
+'
+my_root_depth=$(echo $(realpath "$1" | tr -cd '/' | wc -c))
+for i in $(find $1 -type d); do
+    actual=$(echo $(realpath "$i" | tr -cd '/' | wc -c))
+    result=$(($actual - $my_root_depth))
+    spaces=""
+    for ((j=0;j<result;j++)); do
+        spaces=$spaces"   "
+    done
+    filename=$(basename $i)
+    echo $spaces \* $filename
+done
+
+IFS=$old_ifs
